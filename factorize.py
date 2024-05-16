187c6fe40f50fa8c6f238cead790eca38df57a2c
dosisido <dosisido46@gmail.com>
2024-05-16 14:50:35 +0200

    feat: Add primefac library for prime factorization in factorize.py

--- factorize.py
+++ factorize.py
@@ -3,6 +3,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from Crypto.Util.number import getPrime, getRandomInteger
 from factordb.factordb import FactorDB
 from gmpy2 import next_prime, isqrt
+from primefac import primefac, multifactor
 
 
 n_length = 30
@@ -19,34 +20,20 @@ def factor_db(n):
     factor_list.sort()
     return tuple(factor_list)
 
-
 def fermat(n):
     a = isqrt(n)
     b = a
     b2 = pow(a,2) - n
 
-    # print("a= "+str(a))
-    # print("b= " + str(b))
-
-    # print("b2=" + str(b2))
-    # print("delta-->" + str(pow(b, 2) - b2 % n)+"\n-----------")
-    # print("iterate")
-
     i = 0
     while True:
         if b2 == pow(b,2):
-            # print("found at iteration "+str(i))
             break
         else:
             a+= 1
             b2= pow(a, 2) - n
             b = isqrt(b2)
         i+=1
-    #     print("iteration="+str(i))
-    #     print("a= " + str(a))
-    #     print("b= " + str(b))
-    # print("b2 =" + str(b2))
-    # print("delta-->" + str(pow(b, 2) - b2 % n) + "\n-----------")
 
     p = a+b
     q = a-b
@@ -55,21 +42,37 @@ def fermat(n):
     factor_list.sort()
     return tuple(factor_list)
 
+def primefac_lib(n):
+    primes = []
+    while n != 1:
+        primes.append(multifactor(n)[0])
+        n = n // primes[-1]
+    primes.sort()
+    return tuple(primes)
 
 def main():
     p1 = getPrime(n_length)
     p2 = getPrime(n_length)
     n = p1 * p2
 
+    originals_prime = [p1, p2]
+    originals_prime.sort()
+    print("Original primes: ")
+    print(tuple(originals_prime))
 
-    print("FactorDb: ")
-    print(factor_db(n))
 
-    print('-'*10)
+    # print('-'*10)
+    # print("FactorDb: ")
+    # print(factor_db(n))
 
-    print("Fermat: ")
-    print(fermat(n))
+    # print('-'*10)
+    # print("Fermat: ")
+    # print(fermat(n))
     
+    print('-'*10)
+    print("Primefac: ")
+    print(primefac_lib(n))
+
     pass
 
 if __name__ == "__main__":
