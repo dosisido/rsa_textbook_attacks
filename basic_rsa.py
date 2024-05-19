30ec94cffbe47429b72f531d9fe086c5480db2f0
dosisido <dosisido46@gmail.com>
2024-05-19 21:15:57 +0200

    feat: Update basic_rsa.py to handle both int and bytes message types in encrypt function

--- basic_rsa.py
+++ basic_rsa.py
@@ -1,3 +1,4 @@
+from typing import Union
 from Crypto.PublicKey import RSA
 from Crypto.Util.number import long_to_bytes, bytes_to_long
 from secret import message
@@ -5,8 +6,13 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-def encrypt(message: bytes, key: RSA.RsaKey):
-    return pow(bytes_to_long(message), key.e, key.n)
+def encrypt(message: Union[int, bytes], key: RSA.RsaKey):
+    if isinstance(message, int):
+        return pow(message, key.e, key.n)
+    elif isinstance(message, bytes):
+        return pow(bytes_to_long(message), key.e, key.n)
+    else:
+        raise TypeError("Invalid message type. Expected bytes or int.")
 
 def decrypt(cipher: int, key: RSA.RsaKey):
     return pow(cipher, key.d, key.n)
